d15b8eb79266c3d5a66b8d2e156107f897e24820
unknown <kamran.khan@s.amity.edu>
2024-02-20 00:36:12 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask,request,render_template
+
+obj=Flask(__name__)
+
+@obj.route('/')
+def welcome():
+    return "Welcome to the Flask"
+
+@obj.route('/cal',methods=["GET",])
+def math_operation():
+    operation=request.json["operation"]
+    numbers1=request.json["number1"]
+    numbers2=request.json["number2"]
+
+    if operation=="add":
+        result=numbers1+numbers2
+    elif operation=="multiply":
+        result=numbers1*numbers2
+    elif operation=="division":
+        result=numbers1/numbers2
+    else:
+        result =numbers1-numbers2
+    return result
+
+print(__name__)
+
+if __name__ == '__main__':
+    obj.run()
